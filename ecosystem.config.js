2a10119e912c14211ba79771ecaf1854e402e9b6
anish-singh-rawat <www.computernetwork@gmail.com>
2026-08-17 15:24:54 +0530

    feat : Edit ecosystem cofig code of the project for a 1GB Ram of server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,32 @@
-export default {
+module.exports = {
   apps: [
     {
-      name: 'recharge-crm-backend',
-      script: 'src/server.js',
-      instances: 'max',
-      exec_mode: 'cluster',
-      interpreter: 'node',
-      interpreter_args: '--experimental-vm-modules',
+      name: "recharge-crm-backend",
+      script: "./src/server.js",
+
+      instances: 1,
+      exec_mode: "fork",
+
+      interpreter: "node",
+
       watch: false,
-      max_memory_restart: '500M',
+
+      max_memory_restart: "400M",
       restart_delay: 3000,
       max_restarts: 10,
-      min_uptime: '10s',
+      min_uptime: "10s",
+
       env: {
-        NODE_ENV: 'development',
-        PORT: 8080,
+        NODE_ENV: "production",
+        PORT: 8080
       },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 8080,
-      },
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      error_file: './logs/pm2/error.log',
-      out_file: './logs/pm2/out.log',
-      combine_logs: true,
-      merge_logs: true,
-    },
-  ],
+
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+
+      error_file: "./logs/pm2/error.log",
+      out_file: "./logs/pm2/out.log",
+
+      merge_logs: true
+    }
+  ]
 };
