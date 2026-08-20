4f398069f99720fb13c8029676e180fcc453b1a9
anish-singh-rawat <www.computernetwork@gmail.com>
2026-08-20 18:01:47 +0530

    fix : remove logs related API

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,6 @@ import reportRoutes from './report.routes.js';
 import apiKeyRoutes from './apiKey.routes.js';
 import settingRoutes from './setting.routes.js';
 import providerRoutes from './provider.routes.js';
-import logRoutes from './log.routes.js';
 import healthRoutes from './health.routes.js';
 import externalRoutes from './external.routes.js';
 
@@ -26,7 +25,6 @@ router.use('/reports', reportRoutes);
 router.use('/api-keys', apiKeyRoutes);
 router.use('/settings', settingRoutes);
 router.use('/provider', providerRoutes);
-router.use('/logs', logRoutes);
 router.use('/ext', externalRoutes);
 
 export default router;
